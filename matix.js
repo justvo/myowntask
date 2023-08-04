cbd825182b7f58387eea972d292222a158a13491
Vlad <129364256+justvo@users.noreply.github.com>
2023-08-04 14:59:56 +0300

    add

---
+++ matix.js
@@ -0,0 +1,12 @@
+class Matrix {
+    constructor(arOfNum, line, column){
+        this.arOfNum = arOfNum;
+        this.line = line;
+        this.column = column;
+    }
+    
+additionMatrix(){}
+multipByNumber(){}
+writeMatrix(){}
+multipMatrix(){}
+}
